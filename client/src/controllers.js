e5771d6be0da1823a971b6f6cf91ae5620f97cdb
Nathan Isaac <nathan@nathan-isaac.com>
2014-12-10 01:13:01 -0800

    Add completed alpha version of the app.
    - Add check answers
    - Add reset answers
    - UI enhancements

--- client/src/controllers.js
+++ client/src/controllers.js
@@ -3,33 +3,33 @@ angular.module('ReviewApp')
         $rootScope.PAGE = "all";
 
         $scope.showNames = true;
-        
+
         $scope.types = [];
 
         $scope.quizFilter = "";
 
         $http.get('data/data.json')
-            .success(function(data) {
+            .success(function (data) {
                 $log.debug('Data retreived: ', data);
                 $scope.types = data;
             })
-            .error(function(error) {
+            .error(function (error) {
                 $log.error('An error has occored: ', error);
             });
 
         $scope.nameVisable = true;
 
-        $scope.toggleNames = function() {
+        $scope.toggleNames = function () {
             $scope.showNames = ($scope.showNames == true ? false : true);
         };
 
-        $scope.toggleQuiz = function() {
+        $scope.toggleQuiz = function () {
 
-            switch($scope.quizFilter) {
+            switch ($scope.quizFilter) {
                 case "quiz1":
                     $scope.quizFilter = "quiz2";
                     break;
-                case "quiz2"
+                case "quiz2":
                     $scope.quizFilter = "";
                     break;
                 case "":
@@ -38,5 +38,26 @@ angular.module('ReviewApp')
             }
         };
 
-        
+        $scope.check = function() {
+            angular.forEach($scope.types, function(value, key) {
+                $log.debug('Value, Key: ', value, key);
+                if(angular.lowercase(value.name) !== angular.lowercase(value.answer))
+                {
+                    $scope.types[key].correct = false;
+                }
+                else
+                {
+                    $scope.types[key].correct = true;
+                }
+            });
+
+            $log.debug('Types debug: ', $scope.types);
+        };
+
+        $scope.reset = function() {
+            angular.forEach($scope.types, function(value, key) {
+                $scope.types[key].correct = true;
+                $scope.types[key].answer = "";
+            });
+        }
     });
